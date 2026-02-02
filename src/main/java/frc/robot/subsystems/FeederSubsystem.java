34c7be635c157ad2b97345f69385088ed89008c7
Azura2298 <james-mcewan@outlook.com>
2026-02-02 18:17:17 -0500

    rfgbaesd

--- src/main/java/frc/robot/subsystems/FeederSubsystem.java
+++ src/main/java/frc/robot/subsystems/FeederSubsystem.java
@@ -5,8 +5,8 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.spark.SparkLowLevel.MotorType;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.revrobotics.spark.SparkMax;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.RobotConstants;
 
 public class FeederSubsystem extends SubsystemBase {
