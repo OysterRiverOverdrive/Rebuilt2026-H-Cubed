32a604a5d6e39e8574300c6aa431e17c6ca7d479
27ravez <zachary@jaraven.com>
2026-02-16 18:56:33 -0500

    Add playingwithfusion BFG

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,7 +4,10 @@
 
 package frc.robot;
 
+import com.playingwithfusion.BattFuelGauge;
+
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
@@ -18,6 +21,8 @@ public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
+
+  private final BattFuelGauge fuelGauge = new BattFuelGauge(0);
 
   /**
    * This function is run when the robot is first started up and should be used for any
@@ -44,6 +49,12 @@ public class Robot extends TimedRobot {
     // and running subsystem periodic() methods.  This must be called from the robot's periodic
     // block in order for anything in the Command-based framework to work.
     CommandScheduler.getInstance().run();
+
+    SmartDashboard.putString("Battery ID", fuelGauge.getNickname());
+    SmartDashboard.putNumber("Battery Voltage", fuelGauge.getVoltage());
+    SmartDashboard.putNumber("Battery Charge (%)", fuelGauge.getRemainingChargePct());
+    SmartDashboard.putNumber("Battery Cycle Count", fuelGauge.getNumCycles());
+
   }
 
   /** This function is called once each time the robot enters Disabled mode. */
