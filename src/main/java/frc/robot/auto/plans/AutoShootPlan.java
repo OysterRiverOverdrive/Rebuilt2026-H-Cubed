f5a23018eb2c87805b2c523cc0d59c66a3e96da2
27ravez <zachary@jaraven.com>
2026-03-08 09:13:41 -0400

    Update basic auto

--- src/main/java/frc/robot/auto/plans/AutoShootPlan.java
+++ src/main/java/frc/robot/auto/plans/AutoShootPlan.java
@@ -8,6 +8,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import frc.robot.auto.AutoConstantShootCmd;
 import frc.robot.auto.AutoFeederForwardCmd;
+import frc.robot.auto.AutoIntakeForwardCmd;
 import frc.robot.auto.AutoSleepCmd;
 import frc.robot.commands.intake.IntakeDownCommand;
 import frc.robot.subsystems.FeederSubsystem;
@@ -22,12 +23,14 @@ public class AutoShootPlan extends ParallelCommandGroup {
   public AutoShootPlan(FeederSubsystem feeder, ShooterSubsystem shooter, IntakeSubsystem intake) {
     Command lowerIntake = new IntakeDownCommand(intake);
 
+    Command spinIntake = new AutoIntakeForwardCmd(intake, 6);
+
     Command spinFeeder = new AutoFeederForwardCmd(feeder, 4);
 
     Command constantShoot = new AutoConstantShootCmd(shooter, 6);
 
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
-    addCommands(lowerIntake.andThen(constantShoot.alongWith(new AutoSleepCmd(2).andThen(spinFeeder))));
+    addCommands(lowerIntake.andThen(spinIntake.alongWith(constantShoot.alongWith(new AutoSleepCmd(2).andThen(spinFeeder)))));
   }
 }
