7ef0dd68fc9627bce082f342ba8fe0cc316ce87d
27ravez <zachary@jaraven.com>
2026-03-25 19:58:48 -0400

    PathPlanner Path Finalization
    integration of path planner as auto builder finished along with named command creation for said builder.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,6 +5,7 @@
 package frc.robot;
 
 import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.auto.NamedCommands;
 import com.playingwithfusion.TimeOfFlight;
 import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.DriverStation;
@@ -13,6 +14,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.Constants.DriveConstants;
+import frc.robot.auto.AutoIntakeForwardCmd;
 import frc.robot.auto.plans.AutoAllianceZonePlan;
 import frc.robot.auto.plans.AutoForwardPlan;
 import frc.robot.auto.plans.AutoMiddleFieldPlan;
@@ -75,6 +77,8 @@ public class RobotContainer {
 
     // Declare default command during Teleop Period as TeleopCmd(Driving Command)
     drivetrain.setDefaultCommand(teleopCmd);
+
+    NamedCommands.registerCommand("IntakeForward 5sec", new AutoIntakeForwardCmd(intake, 15.0));
 
     autoChooser = AutoBuilder.buildAutoChooser();
     SmartDashboard.putData("Auto Selector", autoChooser);
